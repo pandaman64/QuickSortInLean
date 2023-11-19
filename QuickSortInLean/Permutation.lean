b5caf71d2922edf14e482a2fc815df30ee8b6f1f
pandaman64 <kointosudesuyo@infoseek.jp>
2023-11-19 23:02:28 +0900

    Use Vec in Permutation

--- QuickSortInLean/Permutation.lean
+++ QuickSortInLean/Permutation.lean
@@ -1,19 +1,4 @@
-import Std.Data.Option.Basic
-import Std.Data.Fin.Basic
-import Std.Data.Array.Lemmas
-
--- Reflexive and transitive closure of arr ⇒ arr.swap i j
-inductive permuted : Array α → Array α → Type where
-  | refl : permuted arr arr
-  | step {arr arr'} (i j : Fin arr.size) : permuted (arr.swap i j) arr' → permuted arr arr'
-
--- theorem permuted_trans : permuted arr arr' → permuted arr' arr'' → permuted arr arr'' := by
---   intro p
---   induction p with
---   | refl => intro; assumption
---   | step _ ih =>
---     intro q
---     exact .step (ih q)
+import QuickSortInLean.Vec
 
 def isInv (f : α → β) (g : β → α) :=
   (∀ x, g (f x) = x) ∧ (∀ y, f (g y) = y)
@@ -29,180 +14,76 @@ theorem invertible_id : invertible (id : α → α) := by
       simp
   exact ⟨id, this⟩
 
-def permuted.to_map (p : permuted arr arr') : Fin arr.size → Fin arr'.size :=
+-- Reflexive and transitive closure of arr ⇒ arr.swap i j
+inductive permuted {α : Type} (n : Nat) : Vec α n → Vec α n → Type where
+  | refl : permuted n arr arr
+  | step {arr arr'} (i j : Fin n) : permuted n (arr.swap i j) arr' → permuted n arr arr'
+
+def permuted.to_map (p : permuted n arr arr') : Fin n → Fin n :=
   match p with
   | .refl => id
   | .step i j p' => fun k =>
-    have h : arr.size = (arr.swap i j).size := by simp
     let k' :=
-      if k.val = i.val then j
-      else if k.val = j.val then i
+      if k = i then j
+      else if k = j then i
       else k
-    p'.to_map (k'.cast h)
+    p'.to_map k'
 
-theorem permuted_size (p : permuted arr arr') : arr'.size = arr.size := by
-  induction p with
-  | refl => rfl
-  | step _ _ _ ih =>
-    simp at ih
-    exact ih
-
-theorem Fin.cast_eq_val {n m : Nat} (h : n = m) (i : Fin n) : (i.cast h).val = i.val := by
-  simp [Fin.cast]
-
-theorem Fin.eq_ite {n : Nat} (i j : Fin n) :
-  (if j.val = i.val then j else i) = i := by
-  cases Nat.decEq j.val i.val with
-  | isFalse h => simp [*]
-  | isTrue h => simp [*, Fin.eq_of_val_eq h]
-
-theorem permuted_map_invertible (p : permuted arr1 arr2) : invertible p.to_map := by
+theorem permuted_map_invertible (p : permuted n arr1 arr2) : invertible p.to_map := by
   induction p with
   | refl => simp [permuted.to_map, invertible_id]
   | @step arr3 arr4 i j p ih =>
-    have size_s3 : (arr3.swap i j).size = arr3.size := by simp
     let ⟨f', ⟨h1, h2⟩⟩ := ih
     let f := permuted.to_map (permuted.step i j p)
     show invertible f
-    let g := fun (k : Fin arr4.size) =>
-      let k := (f' k).cast size_s3
-      -- let k := (f' (f (k.cast size_43))).cast size_s3
-      if k.val = i.val then j
-      else if k.val = j.val then i
-      else k
+    let g := fun (k : Fin n) =>
+      let k' := f' k
+      if k' = i then j
+      else if k' = j then i
+      else k'
     have gf : ∀x, g (f x) = x := by
       intro x
       simp (config := {zeta := false}) [f, permuted.to_map]
-      cases Nat.decEq x.val i.val with
+      cases decEq x i with
       | isFalse nxi =>
-        cases Nat.decEq x.val j.val with
-        | isFalse nxj =>
-          simp (config := {zeta := false}) [nxi, nxj, g]
-          rw [h1]
-          simp [Fin.cast, nxi, nxj]
-        | isTrue xj =>
-          simp (config := {zeta := false}) [nxi, xj, Fin.eq_ite, g]
-          rw [h1]
-          simp [Fin.cast]
-          exact Fin.eq_of_val_eq xj.symm
-      | isTrue xi =>
-        simp (config := {zeta := false}) [xi, g]
-        rw [h1]
-        simp [Fin.cast, Fin.eq_ite]
-        exact Fin.eq_of_val_eq xi.symm
+        cases decEq x j with
+        | isFalse nxj => simp [nxi, nxj, g, h1]
+        | isTrue xj => simp [nxi, xj, g, h1]
+      | isTrue xi => simp [xi, g, h1]
     have fg : ∀y, f (g y) = y := by
       intro y
       simp (config := {zeta := false}) [g]
-      let k : Fin arr3.size := (f' y).cast size_s3
-      show f (if k.val = i.val then j else if k.val = j.val then i else k) = y
-      cases Nat.decEq k.val i.val with
-      | isFalse nki =>
-        cases Nat.decEq k.val j.val with
-        | isFalse nkj =>
-          simp [nki, nkj, permuted.to_map]
-          -- Consective Fin.cast cancel each other
-          show p.to_map (f' y) = y
-          apply h2
-        | isTrue kj =>
-          simp [nki, kj, permuted.to_map,Fin.eq_ite]
-          have : j = k := Fin.eq_of_val_eq kj.symm
-          simp [this]
-          show permuted.to_map p (f' y) = y
-          apply h2
-      | isTrue ki =>
-        simp [ki, permuted.to_map, Fin.eq_ite]
-        have : i = k := Fin.eq_of_val_eq ki.symm
-        simp [this]
-        show permuted.to_map p (f' y) = y
-        apply h2
+      show f (if f' y = i then j else if f' y = j then i else f' y) = y
+      cases decEq (f' y) i with
+      | isFalse nfyi =>
+        cases decEq (f' y) j with
+        | isFalse nfyj => simp [nfyi, nfyj, permuted.to_map, h2]
+        | isTrue fyj =>
+          simp [nfyi, fyj, permuted.to_map]
+          rw [←(congrArg p.to_map fyj), h2]
+      | isTrue fyi =>
+        simp [fyi, permuted.to_map]
+        have : (if j = i then j else i) = i := by simp
+        rw [this, ←(congrArg p.to_map fyi), h2]
     exact ⟨g, ⟨gf, fg⟩⟩
 
-theorem Option.sum_inj {a b : α} : some a = some b → a = b := by simp
-
-theorem getElem?_some_getElem (arr : Array α) (i : Nat) (eq : arr[i]? = some v) :
-  ∃h : i < arr.size, arr[i] = v := by
-  simp [getElem?] at eq
-  cases Nat.decLt i arr.size with
-  | isFalse h => simp [h] at eq
-  | isTrue h =>
-    simp [h] at eq
-    exact ⟨h, eq⟩
-
-theorem Array.eq_get_swap_left (arr : Array α) (i j : Fin arr.size) (h : arr.size = (arr.swap i j).size) :
-  arr[i] = (arr.swap i j)[j.cast h] := by
-  show arr[i] = (arr.swap i j)[(j.cast h).val]
-  have : (arr.swap i j)[(j.cast h).val]? = some arr[i.val] := by
-    rw [Array.get?_swap arr i j (j.cast h).val, Fin.cast_eq_val]
-    simp
-  let ⟨_, eq⟩ := getElem?_some_getElem _ _ this
-  simp [*]
-
-theorem Array.eq_get_swap_right (arr : Array α) (i j : Fin arr.size) (h : arr.size = (arr.swap i j).size) :
-  arr[j] = (arr.swap i j)[i.cast h] := by
-  show arr[j] = (arr.swap i j)[(i.cast h).val]
-  have : (arr.swap i j)[(i.cast h).val]? = some arr[j.val] := by
-    rw [Array.get?_swap arr i j (i.cast h).val, Fin.cast_eq_val]
-    simp
-    intro eq
-    have : j = i := Fin.eq_of_val_eq eq
-    rw [this]
-  let ⟨_, eq⟩ := getElem?_some_getElem _ _ this
-  simp [*]
-
-theorem Array.eq_get_swap_neq (arr : Array α) (i j k : Fin arr.size) (h : arr.size = (arr.swap i j).size) :
-  i ≠ k → j ≠ k → arr[k] = (arr.swap i j)[k.cast h] := by
-  intro ik jk
-  have ikv : i.val ≠ k.val := by
-    intro eq
-    exact ik <| Fin.eq_of_val_eq eq
-  have jkv : j.val ≠ k.val := by
-    intro eq
-    exact jk <| Fin.eq_of_val_eq eq
-  show arr[k] = (arr.swap i j)[(k.cast h).val]
-  have : (arr.swap i j)[(k.cast h).val]? = some arr[k.val] := by
-    rw [Array.get?_swap arr i j (k.cast h).val, Fin.cast_eq_val]
-    simp [ikv, jkv, getElem?, k.isLt]
-  let ⟨_, eq⟩ := getElem?_some_getElem _ _ this
-  simp [*]
-
-theorem permuted_map_index (p : permuted arr1 arr2) (k : Fin arr1.size) :
+theorem permuted_map_index (p : permuted n arr1 arr2) (k : Fin n) :
   arr1[k] = arr2[p.to_map k] := by
-  induction p with
+  induction p generalizing k with
   | refl => simp [permuted.to_map]
   | @step arr3 arr4 i j p ih =>
     simp [permuted.to_map]
-    simp at ih
+    rw [←ih]
     split
-    case step.inl eq =>
-      have : arr3[k.val] = arr3[i.val] := by simp [eq]
-      rw [this]
-      have : arr3.size = (arr3.swap i j).size := by simp
-      rw [←ih]
-      exact Array.eq_get_swap_left arr3 i j this
-    case step.inr neq =>
+    case step.inl ki => simp [ki, Vec.get_swap_right]
+    case step.inr nki =>
       split
-      case inl eq =>
-        have : arr3[k.val] = arr3[j.val] := by simp [eq]
-        rw [this]
-        have : arr3.size = (arr3.swap i j).size := by simp
-        rw [←ih]
-        exact Array.eq_get_swap_right arr3 i j this
-      case inr neq' =>
-        have : arr3.size = (arr3.swap i j).size := by simp
-        rw [←ih]
-        have neq : i ≠ k := by
-          intro eq
-          apply neq
-          simp [eq]
-        have neq' : j ≠ k := by
-          intro eq
-          apply neq'
-          simp [eq]
-        exact Array.eq_get_swap_neq arr3 i j k this neq neq'
+      case inl kj => simp [kj, Vec.get_swap_left]
+      case inr nkj => rw [Vec.get_swap_neq arr3 i j k nki nkj]
 
-theorem permuted_map_index_invertible {arr arr' : Array α} (p : permuted arr arr') :
-  ∃(f : Fin arr.size → Fin arr'.size),
-    invertible f ∧ (∀(i : Fin arr.size), arr[i] = arr'[f i]) := by
+theorem permuted_map_index_invertible {arr arr' : Vec α n} (p : permuted n arr arr') :
+  ∃(f : Fin n → Fin n),
+    invertible f ∧ (∀(i : Fin n), arr[i] = arr'[f i]) := by
   exists p.to_map
   apply And.intro
   . exact permuted_map_invertible p
