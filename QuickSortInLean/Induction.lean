8e527ebec0b935e785f202db739a65d83e227683
pandaman64 <kointosudesuyo@infoseek.jp>
2023-11-24 22:41:42 +0900

    Prove permutation using induction principles

--- QuickSortInLean/Induction.lean
+++ QuickSortInLean/Induction.lean
@@ -1,5 +1,4 @@
 import QuickSortInLean.QuickSort
-import QuickSortInLean.Permutation
 
 theorem partitionImpl.induct {α : Type} [inst : LT α] [DecidableRel inst.lt] {n : Nat}
   (motive : (arr : Vec α n) → (first i j : Nat) → (fi : first ≤ i) → (ij : i ≤ j) → (jn : j < n) → Sort u)
@@ -8,11 +7,11 @@ theorem partitionImpl.induct {α : Type} [inst : LT α] [DecidableRel inst.lt] {
   (base : ∀arr first i j fi ij jn (_ : i < n) (_ : first < n) (_ : ¬first < i), motive arr first i j fi ij jn)
   (step_lt : ∀arr first i j fi ij jn
     (_ : i < n) (_ : first < n) (_ : first < i) (_ : first ≤ i - 1) (_ : arr[i] < arr[first]) (_ : i - 1 ≤ j)
-    (_ : motive arr first (i - 1) j (by assumption) (by assumption) jn),
+    (ih : motive arr first (i - 1) j (by assumption) (by assumption) jn),
     motive arr first i j fi ij jn)
   (step_ge : ∀arr first i j fi ij jn
     (_ : i < n) (_ : first < n) (_ : first < i) (_ : first ≤ i - 1) (_ : ¬arr[i] < arr[first]) (_ : i - 1 ≤ j - 1) (_ : j - 1 < n)
-    (_ : motive (arr.swap ⟨i, by assumption⟩ ⟨j, jn⟩) first (i - 1) (j - 1) (by assumption) (by assumption) (by assumption)),
+    (ih : motive (arr.swap ⟨i, by assumption⟩ ⟨j, jn⟩) first (i - 1) (j - 1) (by assumption) (by assumption) (by assumption)),
     motive arr first i j fi ij jn)
   : motive arr first i j fi ij jn := by
   have : i < n := Nat.lt_of_le_of_lt ij jn
@@ -70,17 +69,56 @@ theorem partitionImpl.simp_step_ge {α : Type} [inst : LT α] [DecidableRel inst
   rw [partitionImpl]
   simp [*, dbgTraceIfShared]
 
-theorem partitionImpl_permuted' {α : Type} [inst : LT α] [DecidableRel inst.lt]
-  {n : Nat} (arr : Vec α n) (first i j : Nat)
-  (fi : first ≤ i) (ij : i ≤ j) (jn : j < n) :
-  permuted n arr (partitionImpl arr first i j fi ij jn).2 := by
-  induction arr, first, i, j, fi, ij, jn using partitionImpl.induct with
-  | base arr first i j fi ij jn =>
-    simp [*]
-    exact .step ⟨first, by assumption⟩ ⟨j, by assumption⟩ .refl
-  | step_lt arr first i j fi ij jn _ _ _ _ _ _ ih =>
-    simp [*]
-    exact ih
-  | step_ge arr first i j fi ij jn _ _ _ _ _ _ _ ih =>
-    simp [*]
-    apply permuted.step ⟨i, by assumption⟩ ⟨j, jn⟩ ih
+theorem quickSortImpl.induct {α : Type} [inst : LT α] [DecidableRel inst.lt] {n : Nat}
+  (motive : (arr : Vec α n) → (first last : Nat) → (ln : last < n) → Sort u)
+  (arr : Vec α n) (first last : Nat) (ln : last < n)
+  (base : ∀arr first last ln (_ : ¬first < last), motive arr first last ln)
+  (step : ∀arr first last ln (lt : first < last)
+    (parted : { mid : Nat // first ≤ mid ∧ mid ≤ last } × Vec α n) (_ : partition arr first last (Nat.le_of_lt lt) ln = parted),
+    have : parted.1 - 1 < n := Nat.lt_of_le_of_lt (Nat.sub_le ..) (Nat.lt_of_le_of_lt parted.1.property.2 ln)
+    ∀(ih₁ : motive parted.2 first (parted.1 - 1) (by assumption))
+      (ih₂ : motive (quickSortImpl parted.2 first (parted.1 - 1) (by assumption)) (parted.1 + 1) last ln),
+    motive arr first last ln
+  )
+  : motive arr first last ln := by
+  if lt : first < last then
+    let parted := partition arr first last (Nat.le_of_lt lt) ln
+    let mid := parted.1.val
+    let hm := parted.1.property
+    have : mid - 1 < n := Nat.lt_of_le_of_lt (Nat.sub_le ..) (Nat.lt_of_le_of_lt hm.2 ln)
+    -- Termination lemmas
+    have : mid - 1 - first < last - first := quickSortImpl.termination_lemma first last lt hm.1 hm.2
+    have : last - (mid + 1) < last - first := Nat.sub_lt_sub_left lt (Nat.lt_of_le_of_lt hm.1 (Nat.lt_succ_self ..))
+
+    apply step arr first last ln lt parted (Eq.refl parted)
+    case ih₁ =>
+      show motive parted.2 first (mid - 1) (by assumption)
+      apply quickSortImpl.induct motive parted.2 first (mid - 1) (by assumption)
+        base step
+    case ih₂ =>
+      show motive (quickSortImpl parted.2 first (mid - 1) (by assumption)) (mid + 1) last ln
+      apply quickSortImpl.induct motive (quickSortImpl parted.2 first (mid - 1) (by assumption)) (mid + 1) last ln
+        base step
+  else
+    apply base arr first last ln lt
+termination_by _ => last - first
+
+@[simp]
+theorem quickSortImpl.simp_base {α : Type} [inst : LT α] [DecidableRel inst.lt]
+  {n : Nat} {arr : Vec α n} {first last : Nat} {ln : last < n}
+  (h : ¬first < last) :
+  quickSortImpl arr first last ln = arr := by
+  unfold quickSortImpl
+  simp [*]
+
+@[simp]
+theorem quickSortImpl.simp_step {α : Type} [inst : LT α] [DecidableRel inst.lt]
+  {n : Nat} {arr : Vec α n} {first last : Nat} {ln : last < n}
+  (lt : first < last) :
+  let parted := partition arr first last (Nat.le_of_lt lt) ln
+  let mid := parted.1.val
+  let hm := parted.1.property
+  have : mid - 1 < n := Nat.lt_of_le_of_lt (Nat.sub_le ..) (Nat.lt_of_le_of_lt hm.2 ln)
+  quickSortImpl arr first last ln = quickSortImpl (quickSortImpl parted.2 first (mid - 1) (by assumption)) (mid + 1) last ln := by
+  rw [quickSortImpl]
+  simp [*]
--- QuickSortInLean/QuickSortPermutation.lean
+++ QuickSortInLean/QuickSortPermutation.lean
@@ -1,73 +1,42 @@
 import QuickSortInLean.Permutation
 import QuickSortInLean.QuickSort
+import QuickSortInLean.Induction
 
-theorem partitionImpl_permuted {α : Type} [Ord α]
+theorem partitionImpl_permuted {α : Type} [inst : LT α] [DecidableRel inst.lt]
   {n : Nat} (arr : Vec α n) (first i j : Nat)
   (fi : first ≤ i) (ij : i ≤ j) (jn : j < n) :
   permuted n arr (partitionImpl arr first i j fi ij jn).2 := by
-  -- Constants
-  have : first < n := Nat.lt_of_le_of_lt (Nat.le_trans fi ij) jn
-  have : i < n := Nat.lt_of_le_of_lt ij jn
-  have : i - 1 ≤ j - 1 := Nat.sub_le_sub_right ij 1
-  have : j - 1 < n := Nat.lt_of_le_of_lt (Nat.sub_le ..) jn
-  let i' : Fin n := ⟨i, by assumption⟩
-  let j' : Fin n := ⟨j, jn⟩
+  induction arr, first, i, j, fi, ij, jn using partitionImpl.induct with
+  | base arr first i j fi ij jn =>
+    simp [*]
+    exact .step ⟨first, by assumption⟩ ⟨j, by assumption⟩ .refl
+  | step_lt arr first i j fi ij jn _ _ _ _ _ _ ih =>
+    simp [*]
+    exact ih
+  | step_ge arr first i j fi ij jn _ _ _ _ _ _ _ ih =>
+    simp [*]
+    apply permuted.step ⟨i, by assumption⟩ ⟨j, jn⟩ ih
 
-  -- Proof
-  unfold partitionImpl
-  match Nat.decLt first i with
-  | isTrue h =>
-    have : first ≤ i - 1 := Nat.le_sub_of_lt h
-    simp [h, dbgTraceIfShared]
-    match hm : compare arr[i] arr[first] with
-    | .lt =>
-      simp [hm]
-      apply partitionImpl_permuted
-    | .eq | .gt =>
-      simp [hm]
-      show permuted n arr (partitionImpl (arr.swap i' j') first (i - 1) (j - 1) _ _ _).2
-      let ih := partitionImpl_permuted (arr.swap i' j') first (i - 1) (j - 1) (by assumption) (by assumption) (by assumption)
-      apply permuted.step i' j' ih
-  | isFalse h =>
-    simp [h, dbgTraceIfShared]
-    exact .step ⟨first, by assumption⟩ j' .refl
-
-theorem partition_permuted {α : Type} [Ord α]
+theorem partition_permuted {α : Type} [inst : LT α] [DecidableRel inst.lt]
   {n : Nat} (arr : Vec α n) (first last : Nat)
   (fl : first ≤ last) (ln : last < n) :
   permuted n arr (partition arr first last fl ln).2 := by
   apply partitionImpl_permuted
 
-theorem quickSortImpl_permuted {α : Type} [Ord α]
+theorem quickSortImpl_permuted {α : Type} [inst : LT α] [DecidableRel inst.lt]
   {n : Nat} (arr : Vec α n) (first last : Nat) (ln : last < n) :
   permuted n arr (quickSortImpl arr first last ln) := by
-  unfold quickSortImpl
-  match Nat.decLt first last with
-  | isTrue h =>
-    simp [h]
-
-    have : first ≤ last := Nat.le_of_lt h
-    let parted := partition arr first last (by assumption) ln
-    let mid := parted.1.val
-    let hm := parted.1.property
-    have : mid - 1 < n := Nat.lt_of_le_of_lt (Nat.sub_le ..) (Nat.lt_of_le_of_lt hm.2 ln)
-    have : mid - 1 - first < last - first := quickSortImpl.termination_lemma first last h hm.1 hm.2
-    have : last - (mid + 1) < last - first := Nat.sub_lt_sub_left h (Nat.lt_of_le_of_lt hm.1 (Nat.lt_succ_self ..))
-
-    show permuted n arr (quickSortImpl (quickSortImpl parted.2 first (mid - 1) (by assumption)) (mid + 1) last ln)
-
-    let partedPermuted : permuted n arr parted.2 := partition_permuted arr first last (by assumption) ln
-    let quickSortImplPermuted₁ := quickSortImpl_permuted parted.2 first (mid - 1) (by assumption)
-    let quickSortImplPermuted₂ := quickSortImpl_permuted
-      (quickSortImpl parted.snd first (mid - 1) (by assumption)) (mid + 1) last (by assumption)
-
-    exact permuted.trans (permuted.trans partedPermuted quickSortImplPermuted₁) quickSortImplPermuted₂
-  | isFalse h =>
-    simp [h]
+  induction arr, first, last, ln using quickSortImpl.induct with
+  | base arr first last ln h =>
+    simp [*]
     exact .refl
-termination_by _ => last - first
+  | step arr first last ln lt parted eq ih₁ ih₂ =>
+    simp [*]
+    let p := partition_permuted arr first last (Nat.le_of_lt lt) ln
+    rw [eq] at p
+    exact permuted.trans (permuted.trans p ih₁) ih₂
 
-theorem quickSort'_permuted {α : Type} [Ord α] {n : Nat} (arr : Vec α n) :
+theorem quickSort'_permuted  {α : Type} [inst : LT α] [DecidableRel inst.lt] {n : Nat} (arr : Vec α n) :
   permuted n arr (quickSort' arr) := by
   simp [quickSort']
   match Nat.decLt 0 n with
@@ -78,7 +47,7 @@ theorem quickSort'_permuted {α : Type} [Ord α] {n : Nat} (arr : Vec α n) :
     simp [h]
     exact .refl
 
-theorem quickSort'_map_index_invertible {α : Type} [Ord α] {n : Nat} (arr : Vec α n):
+theorem quickSort'_map_index_invertible  {α : Type} [inst : LT α] [DecidableRel inst.lt] {n : Nat} (arr : Vec α n):
   ∃f : Fin n → Fin n,
     invertible f ∧ (∀i : Fin n, arr[i] = (quickSort' arr)[f i]) := by
   let p := quickSort'_permuted arr
