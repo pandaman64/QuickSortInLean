c7001b61e0d7deae1af8e9e928cf371f92eb2c5e
pandaman64 <kointosudesuyo@infoseek.jp>
2023-11-26 20:52:44 +0900

    Ranged permutation

--- QuickSortInLean/Permutation.lean
+++ QuickSortInLean/Permutation.lean
@@ -15,34 +15,49 @@ theorem invertible_id : invertible (id : α → α) := by
   exact ⟨id, this⟩
 
 -- Reflexive and transitive closure of arr ⇒ arr.swap i j
-inductive permuted {α : Type} (n : Nat) : Vec α n → Vec α n → Type where
-  | refl : permuted n arr arr
-  | step {arr arr'} (i j : Fin n) : permuted n (arr.swap i j) arr' → permuted n arr arr'
+inductive permuted {α : Type} (n first last : Nat) : Vec α n → Vec α n → Type where
+  | refl : permuted n first last arr arr
+  | step {arr arr'} (i j : Fin n) (fi : first ≤ i) (ij : i ≤ j) (jl : j ≤ last) :
+    permuted n first last (arr.swap i j) arr' → permuted n first last arr arr'
 
-theorem permuted.trans : permuted n arr arr' → permuted n arr' arr'' → permuted n arr arr'' := by
+def permuted.cast_first {α : Type} {n first last first' : Nat} {arr arr' : Vec α n}
+  (self : permuted n first last arr arr') (h : first' ≤ first) :
+  permuted n first' last arr arr' :=
+  match self with
+  | .refl => .refl
+  | .step i j fi ij jl p => .step i j (Nat.le_trans h fi) ij jl (p.cast_first h)
+
+def permuted.cast_last {α : Type} {n first last last' : Nat} {arr arr' : Vec α n}
+  (self : permuted n first last arr arr') (h : last ≤ last') :
+  permuted n first last' arr arr' :=
+  match self with
+  | .refl => .refl
+  | .step i j fi ij jl p => .step i j fi ij (Nat.le_trans jl h) (p.cast_last h)
+
+theorem permuted.trans : permuted n first last arr arr' → permuted n first last arr' arr'' → permuted n first last arr arr'' := by
   intro h
   induction h with
   | refl => intro; assumption
-  | step i j _ ih =>
+  | step i j fi ij jl _ ih =>
     intro h'
-    exact permuted.step i j (ih h')
+    exact permuted.step i j fi ij jl (ih h')
 
-def permuted.to_map (p : permuted n arr arr') : Fin n → Fin n :=
+def permuted.to_map (p : permuted n first last arr arr') : Fin n → Fin n :=
   match p with
   | .refl => id
-  | .step i j p' => fun k =>
+  | .step i j _ _ _ p' => fun k =>
     let k' :=
       if k = i then j
       else if k = j then i
       else k
     p'.to_map k'
 
-theorem permuted_map_invertible (p : permuted n arr1 arr2) : invertible p.to_map := by
+theorem permuted_map_invertible (p : permuted n first last arr1 arr2) : invertible p.to_map := by
   induction p with
   | refl => simp [permuted.to_map, invertible_id]
-  | @step arr3 arr4 i j p ih =>
+  | step i j fi ij jl p ih =>
     let ⟨f', ⟨h1, h2⟩⟩ := ih
-    let f := permuted.to_map (permuted.step i j p)
+    let f := permuted.to_map (permuted.step i j fi ij jl p)
     show invertible f
     let g := fun (k : Fin n) =>
       let k' := f' k
@@ -75,11 +90,11 @@ theorem permuted_map_invertible (p : permuted n arr1 arr2) : invertible p.to_map
         rw [this, ←(congrArg p.to_map fyi), h2]
     exact ⟨g, ⟨gf, fg⟩⟩
 
-theorem permuted_map_index (p : permuted n arr1 arr2) (k : Fin n) :
+theorem permuted_map_index (p : permuted n first last arr1 arr2) (k : Fin n) :
   arr1[k] = arr2[p.to_map k] := by
   induction p generalizing k with
   | refl => simp [permuted.to_map]
-  | @step arr3 arr4 i j p ih =>
+  | step i j fi ij jl p ih =>
     simp [permuted.to_map]
     rw [←ih]
     split
@@ -87,9 +102,9 @@ theorem permuted_map_index (p : permuted n arr1 arr2) (k : Fin n) :
     case step.inr nki =>
       split
       case inl kj => simp [kj, Vec.get_swap_left]
-      case inr nkj => rw [Vec.get_swap_neq arr3 i j k nki nkj]
+      case inr nkj => rw [Vec.get_swap_neq _ i j k nki nkj]
 
-theorem permuted_map_index_invertible {arr arr' : Vec α n} (p : permuted n arr arr') :
+theorem permuted_map_index_invertible {arr arr' : Vec α n} (p : permuted n first last arr arr') :
   ∃(f : Fin n → Fin n),
     invertible f ∧ (∀(i : Fin n), arr[i] = arr'[f i]) := by
   exists p.to_map
--- QuickSortInLean/QuickSortPermutation.lean
+++ QuickSortInLean/QuickSortPermutation.lean
@@ -3,33 +3,35 @@ import QuickSortInLean.QuickSort
 import QuickSortInLean.Induction
 
 theorem partitionImpl_permuted {α : Type} [Ord α]
-  {n : Nat} (arr : Vec α n) (first i j : Nat)
-  (fi : first ≤ i) (ij : i ≤ j) (jn : j < n) :
-  permuted n arr (partitionImpl arr first i j fi ij jn).2 := by
+  {n : Nat} (arr : Vec α n) (first i j last : Nat)
+  (fi : first ≤ i) (ij : i ≤ j) (jl : j ≤ last) (jn : j < n) :
+  permuted n first last arr (partitionImpl arr first i j fi ij jn).2 := by
   induction arr, first, i, j, fi, ij, jn using partitionImpl.induct with
   | base arr first i j fi ij jn =>
     simp [*]
     exact .refl
   | step_lt arr first i j fi ij jn _ _ _ _ _ _ ih =>
     simp [*]
-    exact ih
+    exact ih jl
   | step_ge arr first i j fi ij jn _ _ _ _ _ _ _ ih =>
     simp [*]
-    apply permuted.step ⟨i, by assumption⟩ ⟨j, jn⟩ ih
+    apply permuted.step ⟨i, by assumption⟩ ⟨j, jn⟩ fi ij jl (ih (Nat.le_trans (Nat.sub_le ..) jl))
 
 theorem partition_permuted {α : Type} [Ord α]
   {n : Nat} (arr : Vec α n) (first last : Nat)
   (fl : first ≤ last) (ln : last < n) :
-  permuted n arr (partition arr first last fl ln).2 := by
+  permuted n first last arr (partition arr first last fl ln).2 := by
   let result := partition arr first last fl ln
   let mid := result.1
   simp [partition, dbgTraceIfShared]
-  let p := partitionImpl_permuted arr first last last fl (Nat.le_refl _) ln
-  exact permuted.trans p (.step ⟨first, Nat.lt_of_le_of_lt fl ln⟩ ⟨mid, Nat.lt_of_le_of_lt mid.property.2 ln⟩ .refl)
+  let p := partitionImpl_permuted arr first last last last fl (Nat.le_refl _) (Nat.le_refl _) ln
+  exact permuted.trans p
+    (.step ⟨first, Nat.lt_of_le_of_lt fl ln⟩ ⟨mid, Nat.lt_of_le_of_lt mid.property.2 ln⟩
+      (Nat.le_of_eq (Eq.refl _)) mid.property.1 mid.property.2 .refl)
 
 theorem quickSortImpl_permuted {α : Type} [Ord α]
   {n : Nat} (arr : Vec α n) (first last : Nat) (ln : last < n) :
-  permuted n arr (quickSortImpl arr first last ln) := by
+  permuted n first last arr (quickSortImpl arr first last ln) := by
   induction arr, first, last, ln using quickSortImpl.induct with
   | base arr first last ln h =>
     simp [*]
@@ -38,10 +40,12 @@ theorem quickSortImpl_permuted {α : Type} [Ord α]
     simp [*]
     let p := partition_permuted arr first last (Nat.le_of_lt lt) ln
     rw [eq] at p
-    exact permuted.trans (permuted.trans p ih₁) ih₂
+    exact permuted.trans
+      (permuted.trans p (ih₁.cast_last (Nat.le_trans (Nat.sub_le ..) parted.1.property.2)))
+      (ih₂.cast_first (Nat.le_trans parted.1.property.1 (Nat.le_succ _)))
 
 theorem quickSort'_permuted  {α : Type} [Ord α] {n : Nat} (arr : Vec α n) :
-  permuted n arr (quickSort' arr) := by
+  permuted n 0 (n - 1) arr (quickSort' arr) := by
   simp [quickSort']
   match Nat.decLt 0 n with
   | isTrue h =>
